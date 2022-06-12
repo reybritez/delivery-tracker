3ca7d1fa5184f2f5072e84bd9fad2daf2a62fbf2
Steven Rey Britez <stevenreybritez@gmail.com>
2022-06-12 02:07:26 -0400

    Added changes to db for testing

---
+++ flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    # Crea y configura la app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='st3vdev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        # Carga la configuracion instanciada, si existe cuando no se esta testeando
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        # Carga el test config si ha pasado
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    # Se asegura que la carpeta instanciada exista
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    return app
