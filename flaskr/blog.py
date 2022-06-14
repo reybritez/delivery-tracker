aa49037bd46639732f19ae02661ac88c9e013ffe
Steven Rey Britez <stevenreybritez@gmail.com>
2022-06-13 23:05:56 -0400

    Cambios realizados en inicio webpage

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -10,13 +10,7 @@ bp = Blueprint('blog', __name__, template_folder='templates')
 
 @bp.route('/')
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    return render_template('restaurant/index.html')
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -95,3 +89,18 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+@bp.route('/blog')
+def blog():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/api')
+def api():
+    
+    return render_template('blog/api.html', peticion=peticion)
